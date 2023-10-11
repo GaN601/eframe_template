f841f0f15cfd0186977455af4efa573815bb65cf
GaN601 <gan.n@outlook.com>
2023-10-11 14:35:15 +0800

    discord init

---
+++ discord/src/layout/content_panel.rs
@@ -0,0 +1,52 @@
+use crate::{component, TemplateApp};
+use egui::{Context, InnerResponse};
+
+pub fn default(
+    app: &mut TemplateApp,
+    ctx: &Context,
+    _frame: &mut eframe::Frame,
+) -> InnerResponse<()> {
+    egui::CentralPanel::default().show(ctx, |ui| {
+        component::side_bar::default(app, ui);
+
+        egui::CentralPanel::default().show_inside(ui, |ui| {
+            // The central panel the region left after adding TopPanel's and SidePanel's
+            ui.heading("eframe template");
+
+            ui.horizontal(|ui| {
+                ui.label("Write something: ");
+                ui.text_edit_singleline(&mut app.label);
+            });
+
+            ui.add(egui::Slider::new(&mut app.value, 0.0..=10.0).text("value"));
+            if ui.button("Increment").clicked() {
+                app.value += 1.0;
+            }
+
+            ui.separator();
+
+            ui.add(egui::github_link_file!(
+                "https://github.com/emilk/eframe_template/blob/master/",
+                "Source code."
+            ));
+
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                powered_by_egui_and_eframe(ui);
+                egui::warn_if_debug_build(ui);
+            });
+        });
+    })
+}
+fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
+    ui.horizontal(|ui| {
+        ui.spacing_mut().item_spacing.x = 0.0;
+        ui.label("Powered by ");
+        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+        ui.label(" and ");
+        ui.hyperlink_to(
+            "eframe",
+            "https://github.com/emilk/egui/tree/master/crates/eframe",
+        );
+        ui.label(".");
+    });
+}
---
+++ discord/src/lib.rs
@@ -0,0 +1,7 @@
+#![warn(clippy::all, rust_2018_idioms)]
+
+mod app;
+mod component;
+mod layout;
+
+pub use app::TemplateApp;
