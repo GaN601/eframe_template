bb92212ff050f209fc640ba21a39be6acb9cf7ae
GaN601 <gan.n@outlook.com>
2023-10-17 18:01:21 +0800

    use pname

--- discord/assets/sw.js
+++ discord/assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './{{project-name}}.js',
-  './{{project-name}}_bg.wasm',
+  './{{pname}}.js',
+  './{{pname}}_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
